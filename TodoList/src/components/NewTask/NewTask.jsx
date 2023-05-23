c1ab5fe5d4c8276d86cd94558f059c0a4e3a050e
Santiago Quintero <santiagoquinteroperfil@gmail.com>
2023-05-23 16:31:34 -0500

    reiniciando rama principal

--- TodoList/src/components/NewTask/NewTask.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Task } from '../Task/Task'
-
-export function NewTask ({ task, tasks }) {
-  return task.map((tas, index) => {
-    return <Task task={index + tasks.length + 1} key={index} />
-  })
-}
